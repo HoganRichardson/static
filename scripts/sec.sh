1cad221974c50e006493feb601cac69e7ceda376
Hogan Richardson <hogi.music@gmail.com>
2019-08-18 15:54:57 +1000

    Updated scripts

---
+++ scripts/sec.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+echo "Installing pwntools..."
+sudo apt-get update && \
+sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && \
+sudo pip install --upgrade pip && \
+sudo pip install --upgrade pwntools
+
+echo "Installing pwndbg..."
+git clone https://github.com/pwndbg/pwndbg && \
+cd pwndbg && \
+./setup.sh 
+
+cd .. && \ 
+rm -r pwndbg
