0dc935d37a3a1097b4be4086cef43721861891e5
HoganRichardson <h.richardson@student.unsw.edu.au>
2019-09-03 10:57:23 +1000

    added -y

--- scripts/sec.sh
+++ scripts/sec.sh
@@ -2,7 +2,7 @@
 
 echo "Installing pwntools..."
 sudo apt-get update && \
-sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && \
+sudo apt-get install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && \
 sudo pip install --upgrade pip && \
 sudo pip install --upgrade pwntools
 
