f4a0512a56dcfcd80f0e8d277de94731094cfd06
Hogan Richardson <hogi.music@gmail.com>
2020-06-17 09:43:06 +1000

    Added script

---
+++ scripts/nginxgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Usage ./nginxgen.sh HOSTNAME PATH
+
+hostname=$1
+app_path=$2
+
+echo "NGINX App Generator"
+
+echo "   -- Creating App with hostname $hostname"
+echo "   -- Using UWSGI path $app_path/myapp.sock"
+
+sudo echo "server {
+	listen 80;
+	server_name $hostname;
+	
+	location / {
+		include 	uwsgi_params;
+		uwsgi_pass	unix:$app_path/myapp.sock;
+	}
+}" > /etc/nginx/sites-available/uwsgi_app
+
+echo "   -- Enabling Site"
+sudo ln -s /etc/nginx/sites-available/uwsgi_app /etc/nginx/sites-enabled
+
+echo "   NGINX Complete"
