93afbc75c01b951328feb5f008459c3801df63c7
Hogan Richardson <hogi.music@gmail.com>
2020-07-02 15:41:57 +1000

    Added https script

---
+++ scripts/nginxgenhttps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Usage ./nginxgen.sh HOSTNAME PATH
+
+hostname=$1
+app_path=$2
+
+echo "NGINX App Generator"
+
+echo "   -- Creating App with hostname $hostname"
+echo "   -- Using UWSGI path $app_path/myapp.sock"
+
+echo "server {
+	listen 443 ssl default_server;
+	server_name localhost,$hostname;
+	
+	location / {
+		include 	uwsgi_params;
+		uwsgi_pass	unix:$app_path/web.sock;
+	}
+}
+server {
+	listen 80;
+	
+}" > /etc/nginx/sites-available/uwsgi_app
+
+echo "   -- Enabling Site"
+ln -s /etc/nginx/sites-available/uwsgi_app /etc/nginx/sites-enabled
+
+echo "   -- Removing default site"
+rm /etc/nginx/sites-enabled/default
+
+echo "   NGINX Complete"
